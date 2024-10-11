8128df21d2d9614a6adb5f3a3140dea8dbde18cb
manuzam <142548975+manuzam@users.noreply.github.com>
2024-10-11 11:44:46 -0300

    kjkj

---
+++ perguntas.js
@@ -0,0 +1,99 @@
+
+criarCartao(
+'Física' ,
+'Oq é velocidade?' ,
+'Velocidade é a relação entre a distância percorrida e o tempo gasto')
+
+criarCartao(
+'Química',
+'O que é água (H₂O)? ',
+'Água é uma substância composta por dois átomos de hidrogênio e um átomo de oxigênio.')
+
+criarCartao(
+'Matemática',
+'O que é a raiz de uma equação quadrática?',
+'A raiz de uma equação quadrática são os valores de x que satisfazem a equação 𝑎𝑥²+bx+c=0. São os pontos onde o gráfico da função cruza o eixo x.')
+
+criarCartao(
+'Biologia',
+'Qual é a principal função das folhas nas plantas?',
+'Realizar a fotossíntese.')
+
+criarCartao(
+'Gografia',
+'Qual é o continente onde fica o Brasil?',
+'América do Sul.')
+
+criarCartao(
+'Historia',
+'Quem foi o primeiro imperador do Brasil?'),
+
+criarCartao(
+'Filosofia',
+' O que é sabedoria?',
+'Sabedoria é o conhecimento profundo que leva à tomada de boas decisões.')
+
+criarCartao(
+'Literatura' ,
+'Quem escreveu "Os Lusíadas"?',
+'Luís de Camões')
+
+criarCartao(
+'Sociologia',
+' O que é sociedade?',
+'Sociedade é um conjunto de indivíduos que compartilham cultura, leis e instituições.')
+
+criarCartao(
+'Física',
+'Qual é a unidade de medida de massa no Sistema Internacional?',
+'Quilograma (kg).')
+
+criarCartao(
+'Química',
+'Qual é o estado físico da água a 100°C em condições normais?',
+'Gasoso (vapor)')
+
+criarCartao(
+'Biologia',
+'Qual é o nome da célula reprodutiva masculina?',
+'Espermatozoide')
+
+criarCartao(
+'Geografia' ,
+'Qual é a capital do Brasil?',
+'Brasília')
+
+criarCartao(
+'História',
+'Em que ano o Brasil declarou independência?',
+'1822')
+
+criarCartao(
+'Filosofia' ,
+' O que é o conhecimento?',
+'Conhecimento é a compreensão ou entendimento de algo adquirido através do estudo, experiência ou percepção')
+
+criarCartao(
+'Literatura',
+'Quem é o autor de "Dom Casmurro"?',
+'Machado de Assis')
+
+criarCartao(
+'Sociologia' ,
+'O que é família ?' ,
+'Família é um grupo social formado por pessoas com laços de parentesco')
+
+criarCartao(
+'Química',
+'O que é o gás oxigênio (O₂)? ',
+'Gás oxigênio é um elemento químico essencial para a respiração dos seres vivos.')
+
+criarCartao(
+'Matemática',
+'O que é uma função linear?',
+'Uma função linear é uma função da forma f(x)=ax+b onde a e b são constantes. Seu gráfico é uma reta.')
+
+criarCartao(
+'Matemática' ,
+'Qual é a fórmula da soma dos ângulos internos de um triângulo?',
+'A soma dos ângulos internos de um triângulo é sempre 180∘')
