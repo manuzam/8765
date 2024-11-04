22661675e95db1d43d35d1254b5ef8a510ffec43
manuzam <142548975+manuzam@users.noreply.github.com>
2024-11-04 08:29:40 -0300

    .

--- projeto2.js
+++ projeto2.js
@@ -1,17 +1,31 @@
-function criarCartao(categoria, pergunta, resposta){
-    let container = document.getElementById('container');
-    let cartao = document.createElement('article');
-    cartao.className = 'cartao';
-    cartao.innerHTML = `<div class="conteudo-cartao">
-    <h3>${categoria}</h3>
-    <div class="pergunta-cartao">
-        <p>${pergunta}</p>
+function criaCartao (categoria, pergunta, resposta){
+    let container = document.getElementById ('container');
+    let cartao = document.createElement ('article');
+    cartao.className= 'cartao'
+    cartao.innerHTML =`
+    <div class="conteudo-cartao" onclick = "mostraResposta(this)" >
+        <h3>${categoria}</h3>
+         <div class="pergunta-cartao">
+            <p>${pergunta}</p>
+         </div>
+         <div class="resposta-cartao">
+            <p>${resposta}</p>
+         </div>
     </div>
-    <div class="resposta-cartao">
-        <p>${resposta}</p>
-    </div>
-</div>
-`
-container.appendChild(cartao);
-}
+    `
+    
+    container.appendChild(cartao);
+    }
+    
+    function mostraResposta(card){
 
+      const resposta = card.querySelector('.resposta-cartao')
+      if(resposta.style.display === 'none'){
+resposta.style.display = 'block'
+      }
+      else{ resposta.style.display = 'none'
+
+      }
+
+    }
+    
