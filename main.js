e21181180fab5d7701bfe589d0d46ab179fec700
manuzam <142548975+manuzam@users.noreply.github.com>
2024-11-04 08:28:35 -0300

    .

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 function criarCartao(categoria, pergunta, resposta){
     let container = document.getElementById('container');
     let cartao = document.createElement('article');
-    cartao.className = 'cartao';
-    cartao.innerHTML = `<div class="conteudo-cartao">
+    cartao.className = 'cartao'
+    cartao.innerHTML =`
+    <div class="conteudo-cartao">
     <h3>${categoria}</h3>
     <div class="pergunta-cartao">
         <p>${pergunta}</p>
